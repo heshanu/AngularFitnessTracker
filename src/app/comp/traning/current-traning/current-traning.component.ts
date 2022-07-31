95ad3fe9bf1e1fe6dfe5076bf3ff0de665f4c12a
heshanu <heshanu97@gmail.com>
2022-08-01 04:26:33 +0530

    Added exit continue option

--- src/app/comp/traning/current-traning/current-traning.component.ts
+++ src/app/comp/traning/current-traning/current-traning.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTraningCompoent } from './stop-traning.compoent';
 @Component({
@@ -7,11 +7,11 @@ import { StopTraningCompoent } from './stop-traning.compoent';
   styleUrls: ['./current-traning.component.css'],
 })
 export class CurrentTraningComponent implements OnInit {
-  @Output() traningExit=new EventEmitter();
+  @Output() traningExit = new EventEmitter();
   progress = 0;
   timer!: any;
 
-  constructor(private dialog:MatDialog ) {
+  constructor(private dialog: MatDialog) {
     this.startOrResume();
   }
 
@@ -21,34 +21,35 @@ export class CurrentTraningComponent implements OnInit {
       console.log(this.progress);
       if (this.progress >= 100) {
         clearInterval(this.timer);
+        //this.progress=0;
       }
     }, 1000);
   }
 
-  startOrResume(){
+  startOrResume() {
     this.timer = setInterval(() => {
-      this.progress = this.progress + 5;
-
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);
+    });
   }
 
-  onStop(){
+  onStop() {
     clearInterval(this.timer);
-    const dialogRef=this.dialog.open(StopTraningCompoent,{data:{
-      progress:this.progress
-    }});
-
-    dialogRef.afterClosed().subscribe(result=>{
-      console.log(result);
-      if(result){
-        this.traningExit.emit();
-      }else{
-        this.startOrResume();
-      }
+    const dialogRef = this.dialog.open(StopTraningCompoent, {
+      data: {
+        progress: this.progress,
+      },
     });
 
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.traningExit.emit();
+      } else {
+       // this.startOrResume();
+        this.timer+=5;
+      }
+    });
   }
 }
