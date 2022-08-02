95d8dd81966e17974d18081ac622e0afcc686ffc
heshanu <heshanu97@gmail.com>
2022-08-03 04:31:38 +0530

    make code neat in snakebar inside uiservice

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,37 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs/Subject';
-import { AngularFireAuth } from 'angularfire2/auth';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs/Subject";
+import { AngularFireAuth } from "angularfire2/auth";
 
-import { User } from './user.model';
-import { AuthData } from './auth-data.model';
-import { TrainingService } from '../training/training.service';
-import { MatSnackBar } from '@angular/material';
-import { UIService } from '../shared/ui.service';
+import { User } from "./user.model";
+import { AuthData } from "./auth-data.model";
+import { TrainingService } from "../training/training.service";
+import { MatSnackBar } from "@angular/material";
+import { UIService } from "../shared/ui.service";
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
- e:boolean=false;
+  e: boolean = false;
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
-    private snackbar:MatSnackBar,
-    private uiservice:UIService
+    private uiservice: UIService
   ) {}
 
   initAuthListener() {
-    this.afAuth.authState.subscribe(user => {
+    this.afAuth.authState.subscribe((user) => {
       if (user) {
         this.isAuthenticated = true;
         this.authChange.next(true);
-        this.router.navigate(['/training']);
+        this.router.navigate(["/training"]);
       } else {
         this.trainingService.cancelSubscriptions();
         this.authChange.next(false);
-        this.router.navigate(['/login']);
+        this.router.navigate(["/login"]);
         this.isAuthenticated = false;
       }
     });
@@ -41,15 +40,17 @@ export class AuthService {
     this.uiservice.loadingStateChanged.next(true);
     this.afAuth.auth
       .createUserWithEmailAndPassword(authData.email, authData.password)
-      .then(result => {
+      .then((result) => {
         this.uiservice.loadingStateChanged.next(false);
       })
-      .catch(error => {
+      .catch((error) => {
         this.uiservice.loadingStateChanged.next(false);
         console.log(error);
+        this.uiservice.showSnakeBar(error,null,3000);
+        /*
         this.snackbar.open(error.message,null,{
           duration:3000
-        })
+        })*/
       });
   }
 
@@ -58,17 +59,19 @@ export class AuthService {
     this.uiservice.loadingStateChanged.next(true);
     this.afAuth.auth
       .signInWithEmailAndPassword(authData.email, authData.password)
-      .then(result => {
+      .then((result) => {
         console.log(result);
         this.uiservice.loadingStateChanged.next(false);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         //adeed spiinner
+        this.uiservice.showSnakeBar(error,null,3000);
         this.uiservice.loadingStateChanged.next(false);
-        this.snackbar.open(error.message,null,{
-          duration:3000
-        })
+        /*
+        this.snackbar.open(error.message, null, {
+          duration: 3000,
+        });*/
       });
   }
 
--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,4 +1,17 @@
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+import { MatSnackBar } from '@angular/material';
+
+@Injectable()
 export class UIService{
     loadingStateChanged=new Subject<boolean>();
+
+    constructor(private snakebar:MatSnackBar){}
+
+    //snakebar further user friendly
+    showSnakeBar(message,action,duration){
+this.snakebar.open(message,action,{
+  duration:duration
+})
+    }
 }
